69ea9c43c9bf9ad68132d86264c27545bd7e9189
RonnyRitscher <r.k.ritscher@web.de>
2019-03-07 09:28:34 +0100

    2019.03.07_09:30

--- app/src/main/java/de/proneucon/mysensorsdemo/MainActivity.java
+++ app/src/main/java/de/proneucon/mysensorsdemo/MainActivity.java
@@ -4,13 +4,16 @@ package de.proneucon.mysensorsdemo;
  */
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorEventListener2;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialisieren der Member:
         tv_main = findViewById(R.id.tv_main);
     }
 
@@ -51,17 +55,56 @@ public class MainActivity extends AppCompatActivity {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 isDynamic = sensor.isDynamicSensor(); // Dynamische Sensoren sind aufsetzbare/externe Sensoren(zB:Wettersensor)
             }
-            //wenn sensor vorhanden, dann infos übergeben:
+            //wenn sensor vorhanden, dann infos übergeben: (%1$s (Hersteller: %2$s, Version: %3$d, dynamisch: %4$s)\n)
             tv_main.append(getString(R.string.template,
-                    sensor.getName(),
-                    sensor.getVendor(),
-                    sensor.getVersion(),
-                    Boolean.toString(isDynamic)
+                    sensor.getName(),               //Name
+                    sensor.getVendor(),             //Hersteller
+                    sensor.getVersion(),            //Version
+                    Boolean.toString(isDynamic)     //dynamisch
             ));
+        }
+
+        //der sensor hat hier bereits die gültigkeit verloren
+        //daher noch einmal direkt ansteuern
+        sensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT); // wir verwenden den Licht-Sensor
+        //zur sicherheit abprüfen ob diesen vorhanden ist:
+        if(sensor == null){
+            Toast.makeText(this, "Kein Lichtsensor vorhanden!", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Lichtsensor vorhanden", Toast.LENGTH_SHORT).show();
+
+            //EventListener erstellen und verwenden:
+            listener = new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {        //wenn sich im sensor selbst Werte ändern (zB: das Licht wird heller)
+                    //values beinhaltet die Änderungen:
+                    if(event.values.length>0){
+
+                        int i=1;
+                        for(float f : event.values){
+                            Log.d(TAG, "onSensorChanged: " +i+ ": " + f);
+                            i++;
+                            /*
+                            1.Wert: Lux / Lichteinfall
+                            2.Wert: Celvin / Farbtemparatur
+                            3.Wert:
+                             */
+                        }
+
+
+                    }
+                }
+                //
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            };
+            //registrieren des Listeners:  *in der onPause wieder unregistrieren
+            manager.registerListener(listener , sensor, SensorManager.SENSOR_DELAY_NORMAL); //
 
 
         }
-
     }
 
     //------------------------------------
@@ -69,5 +112,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        manager.unregisterListener(listener); //Listener unregistrieren
     }
 }
